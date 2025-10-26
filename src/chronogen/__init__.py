b4e3b4b128e363c7b48eeab2d247b6fc17bd66db
Gauthier Le Pape <galep@galep.dev>
2025-10-26 22:25:06 +0100

    Add --version command

--- src/chronogen/__init__.py
+++ src/chronogen/__init__.py
@@ -1,5 +1,7 @@
 """High level API for generating date wordlists and iterators."""
 
+from importlib import metadata
+
 from .core import (
     DateGenerator,
     DateGeneratorConfig,
@@ -15,3 +17,9 @@ __all__ = [
     "generate_dates",
     "parse_format_spec",
 ]
+
+try:
+    __version__ = metadata.version("chronogen")
+except metadata.PackageNotFoundError:
+    # Fallback for development (not installed)
+    __version__ = "dev"
